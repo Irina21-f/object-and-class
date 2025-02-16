489bb67f829d4e074749ab3b1530336031b0182d
ira <ilovekfc45@gmail.com>
2025-02-16 17:40:05 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        String nameJohn = "";
-        String[] names = {"John", "Sarah"};
-        int[] ages = {13, 30};
-        for (int i = 0; i < names.length; i++) {
-            System.out.printf("Name - %s \nAge - %s\n", names[i], ages[i]);
-        }
-        person sarah = new person();
-        sarah.name = "Sarah";
-        System.out.println("sarah.name = " + sarah.name);
-        sarah.age = 12;
-        System.out.println("sarah.age = " + sarah.age);
+        //первая книга с измененным годом
+        Author pushkin = new Author("Александр", "Пушкин");
+        System.out.println("Имя автора - " + pushkin.getNameAuthor());
+        System.out.println("Фамилия автора - " + pushkin.getSurnameAuthor());
+
+        Book onegin = new Book("Евгений Онегин", pushkin, 1950);
+        System.out.println("Название книги " + onegin.getNameOfBook());
+        System.out.println("Писатель " + onegin.getAuthor());
+        System.out.println("Год выпуска " + onegin.getPublishingYear());
+        onegin.setPublishingYear(2000);
+        System.out.println("Год выпуска " + onegin.getPublishingYear());
+        //вторая книга
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        System.out.println("Имя автора - " + lermontov.getNameAuthor());
+        System.out.println("Фамилия автора - " + lermontov.getSurnameAuthor());
+        Book parus = new Book("Парус", lermontov, 2001);
+        System.out.println("Название книги " + parus.getNameOfBook());
+        System.out.println("Писатель " + parus.getAuthor());
+        System.out.println("Год выпуска " + parus.getPublishingYear());
     }
 }
